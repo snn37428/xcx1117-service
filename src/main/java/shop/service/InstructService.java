40e996d990df9c4a60f6e9542c0007cd14ed299b
snn37428 <snn_snnn@163.com>
2018-12-01 01:22:26 +0800

    配置mq

--- src/main/java/shop/service/InstructService.java
+++ src/main/java/shop/service/InstructService.java
@@ -2,10 +2,18 @@ package shop.service;
 
 import org.apache.log4j.Logger;
 import org.springframework.util.StringUtils;
+import shop.dao.InstructMapper;
+import shop.domain.Instruct;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 public class InstructService {
 
     private static final Logger logger = Logger.getLogger(InstructService.class);
+
+    @Resource
+    private InstructMapper instructMapper;
 
     public void write(String addrsess, String status) {
 
@@ -13,6 +21,19 @@ public class InstructService {
             logger.warn("write: param is invalid");
             return;
         }
+
+        Instruct instruct = new Instruct();
+        instruct.setModbusAddr(Integer.parseInt(addrsess));
+        instruct.setStatus(Integer.parseInt(status));
+        instruct.setCreated(new Date());
+
+        int rs = instructMapper.insert(instruct);
+
+        if (rs > 0) {
+            logger.warn("write: insert is success");
+        }
+
+
 
         logger.warn("write: param is invalid");
 
